2323f52ff375a1a68bbb3a1b25d6fc25a56d3f92
kontroldev <raulgallego79@icloud.com>
2023-07-25 12:33:20 +0200

    Añadido clases de estructuras y clases.
    Datos por valor y datos por referencia.

--- Orientada a Objetos en Swift.playground/Pages/Estructuras y clases.xcplaygroundpage/Contents.swift
+++ Orientada a Objetos en Swift.playground/Pages/Estructuras y clases.xcplaygroundpage/Contents.swift
@@ -27,7 +27,7 @@ class SomeClass {        // Asi de define una clase.
 }
 
 
-struct Resolution {   // La estructura tiene en este caso dos propiedades(width, height) estas se llaman `Stored Properties`, forman parte de la estructura.
+struct Resolution {   // La estructura tiene en este caso dos propiedades(width, height) estas se llaman `Stored Properties`, forman parte de la estructura y puedes ser variables en tiempo de ejecucion.
     var width = 0
     var height = 0
 }
@@ -37,4 +37,61 @@ class VideoMode {
     var interlaced = false
     var frameRate = 0.0
     var name: String?
+}
+
+let someResolution = Resolution() // Esto crea un obejeto de la estructra o de la clase para ser usado en codigo.
+let someVideoMode = VideoMode()       // Esto tambien s epuede llamar un `Init`
+
+print(someResolution.width)  //La funcion `.width` accede a la funcion de la `Resolution` - "computed properties",para preguntar la resolucion de esa funcion.
+someVideoMode.resolution.width = 1280  // Cambia el valor. de `someVideMode` - `resolucion`y accedo con el `.width`
+print(someVideoMode.resolution.width)
+
+
+/* NOTA IMPORTANTE
+ 
+ - La instancia que sea constente nunca podra cambiar si estado por ser una "constante" pero, si las variables qued tenga en su interior `Store Properties`-`computed properties`si podran hacerlo
+ 
+ UNA ESTRUCTURA DECLARADA COMO CONSTANTE NO PODRÁ CAMBIAR SUS VALORES, PERO SI LA INSTANCIA DE UN OBJETO DENTRO DE ELLA.*/
+
+
+//ESTRUCTURAS: DATOS COPIADOS POR VALOR ⬇️
+
+let vga = Resolution(width: 640, height: 480)
+vga.width
+vga.height
+
+let hd = Resolution(width: 1920, height: 1080)
+
+var cinema = hd
+print("\(cinema.width) x \(cinema.height)")
+cinema.width = 2048
+print("\(cinema.width) x \(cinema.height)")
+print("\(hd.width) x \(hd.height)")
+
+enum CompassPoint {
+    case north, south, east, west
+}
+
+var currentDirection = CompassPoint.north
+let oldDirection = currentDirection   // `oldDirection` es una copia de `currentDirection`
+currentDirection = .south
+
+print(currentDirection)
+print(oldDirection)
+
+
+// CLASES: DATOS POR REFERENCIAS
+
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+
+if tenEighty === alsoTenEighty {  // Para comparar dos objetos, en est caso en vez de usar 2 `==` vamos a tener que usar 3 `===` para hacer la conmporapacion.
+    print("Son el mismo objeto")     // Y si ponemos !== estamos diciendo la no igualdad.
+}else{
+    print("No son el mismo objeto")
 }
